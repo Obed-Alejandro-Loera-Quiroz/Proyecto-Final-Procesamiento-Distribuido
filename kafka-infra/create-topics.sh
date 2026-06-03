7540f0491a6384c8981800bd0c147d51b34903a2
Obed Alejandro Loera Quiroz <obedloera21@gmail.com>
2026-06-03 15:54:14 -0600

    Correccion de los topicos de sh

--- kafka-infra/create-topics.sh
+++ kafka-infra/create-topics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Apuntamos a tu IP local del módem
+# Apuntamos a tu IP local fija del módem
 BROKER="192.168.0.103:9092"
 
 echo "========================================================="
@@ -11,16 +11,16 @@ crear_topico() {
     NOMBRE_TOPICO=$1
     echo "-> Creando tópico: ${NOMBRE_TOPICO}..."
     
-    # Añadimos la ruta '/bin/' antes del comando para que Docker lo encuentre sí o sí
-    docker compose exec kafka-nodo /bin/kafka-topics.sh \
+    # Se usa docker exec directo apuntando al contenedor real: kafka-cluster-nodo-3
+    docker exec kafka-cluster-nodo-3 kafka-topics \
         --bootstrap-server $BROKER \
         --create \
         --topic $NOMBRE_TOPICO \
         --partitions 3 \
-        --replication-factor 1
+        --replication-factor 3
 }
 
-# --- LOS 5 TÓPICOS ---
+# --- LOS 5 TÓPICOS SOLICITADOS ---
 crear_topico "personas-registro"
 crear_topico "personas-activas"
 crear_topico "personas-ingresos"
@@ -32,4 +32,4 @@ echo "¡Estructura de tópicos inicializada localmente!"
 echo "========================================================="
 
 echo "Tópicos activos en tu clúster actual:"
-docker compose exec kafka-nodo /bin/kafka-topics.sh --bootstrap-server $BROKER --list
+docker exec kafka-cluster-nodo-3 kafka-topics --bootstrap-server $BROKER --list
